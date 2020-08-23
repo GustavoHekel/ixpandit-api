509d3e928dc88cb64af588aad20f97d98a684593
gustavohekel <gustavo.hekel@gmail.com>
2020-08-23 15:06:23 -0300

    Add comments

--- app/Http/Controllers/API/V1/PokemonController.php
+++ app/Http/Controllers/API/V1/PokemonController.php
@@ -14,7 +14,13 @@ class PokemonController extends Controller
         $queryString = array_merge(
             $request->all(),
             [
-                'limit' => 890
+                'limit' => 807
+                /*
+                 * I'm fetching the first 807 pokemon (of a total of 1048 available on the API)
+                 * I've taken this approach as there's no way to make a partial search by name on the
+                 * pokemon API, so I get the full list, save it in cache and then filter by name, then from that
+                 * smaller set I fetch the details of each one (and again store that result in cache)
+                 */
             ]
         );
 
